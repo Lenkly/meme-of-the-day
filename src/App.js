7b1d0dd8f95fe75a6d0a2ca7a52fe8552550e54d
Lenkly <lenk.lydia@gmail.com>
2022-09-08 21:53:52 +0200

    :sparkles: add random color generator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { getRandomColor } from "./lib/randomColor";
 
 function App() {
+  const [randomColor, setRandomColor] = useState("");
+
+  useEffect(() => {
+    setRandomColor(getRandomColor);
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: `${randomColor}` }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meme of the Day</h1>
       </header>
     </div>
   );
